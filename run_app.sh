b784e5693fc6574d724fbf893da6ad35c2bdc64e
BW <boris@victorov.com>
2026-06-29 12:12:25 +0300

    Updated run_app sh

--- run_app.sh
+++ run_app.sh
@@ -3,6 +3,12 @@ set -e
 # Шаг 1: загрузка данных в HDFS (если нужен)
 kubectl apply -f ./pvc.yaml
 kubectl apply -f ./hadoop/kuber
+kubectl wait --for=condition=available deployment/namenode --timeout=600s
+kubectl wait --for=condition=available deployment/nodemanager --timeout=600s
+kubectl wait --for=condition=available deployment/resourcemanager --timeout=600s
+kubectl wait --for=condition=available deployment/datanode1 --timeout=600s
+POD=$(kubectl get pod -l io.kompose.service=namenode -o jsonpath='{.items[0].metadata.name}')
+kubectl cp data/food.parquet "$POD:/data/food.parquet" -c namenode
 sleep 90
 kubectl apply -f loader_job.yaml
 kubectl wait --for=condition=complete job/hdfs-upload-food --timeout=600s
